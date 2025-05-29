6d3c0b868cc467001d8b5bbdeea18bd8b2716344
niluws <ns1378@yahoo.com>
2025-05-29 14:26:10 +0330

    best way

--- Reverse String.py
+++ Reverse String.py
@@ -1,10 +1,6 @@
 class Solution:
     def reverseString(self, s) -> None:
-        j = 0
-
-        for i in s[::-1]:
-            s[j] = i
-            j += 1
+        s[:]=s[::-1]
         return s
 
 
