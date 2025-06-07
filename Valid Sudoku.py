8eb646bc503bd3cd569a8592e64922f2dc0b35bc
niluws <ns1378@yahoo.com>
2025-06-08 01:28:09 +0330

    isValidSudoku

---
+++ Valid Sudoku.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == '.':
+                    continue
+
+
+                box_index = (r // 3) * 3 + (c // 3)
+
+                if val in rows[r] or val in cols[c] or val in boxes[box_index]:
+                    return False
+
+                rows[r].add(val)
+                cols[c].add(val)
+                boxes[box_index].add(val)
+
+        return True
+
+
+Solution().isValidSudoku(
+    [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+        , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+        , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+        , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+        , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+        , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+        , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+        , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+        , [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
+
+
