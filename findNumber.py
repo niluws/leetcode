d5950c4673660246c27c7c09e5236187eab5a6f2
niluws <ns1378@yahoo.com>
2025-06-14 15:51:47 +0330

    findNumbers

---
+++ findNumber.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+
+        even = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                even += 1
+        return even
+
+
+print(Solution().findNumbers([12, 345, 2, 6, 7896]))
